bfd8b293ebe3f73d6aff97d6cf95fd5a8b4e76e7
kamrul <kamrul_sscis@hotmail.com>
2019-06-10 20:03:04 +0100

    Deleting product with quantity option

--- app/Http/Controllers/Product/ProductSerialController.php
+++ app/Http/Controllers/Product/ProductSerialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Product;
 use App\ProductSerial;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -10,8 +11,15 @@ class ProductSerialController extends Controller
 {
     //
 
-    public function destroy($id){
+    public function destroy(Request $request, $id){
         $productSerial = ProductSerial::findOrFail($id);
+        // If type='quantity' then remove quantity form product
+        if($request->type === 'quantity'){
+           $product = Product::findOrFail($productSerial->product_id);
+           $product->quantity = $product->quantity - 1; // Reduce one product
+            $product->save();
+        }
+
         $productSerial->delete();
     }
 }
